a50622e3741473fcdd0bc5020d4c1382ab03f2f3
Rocia <rocia.fernandes@gmail.com>
2017-07-13 11:31:55 +0530

    Queue Pipe and basic process examples

---
+++ mpbasic2.py
@@ -0,0 +1,9 @@
+from multiprocessing import Process
+
+def f(name):
+    print ('Hi', name, '\nHave a nice day')
+
+if __name__ == '__main__':
+    p = Process(target=f, args=('Alloy',))
+    p.start()
+    p.join()
